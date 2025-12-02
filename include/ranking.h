c86b27ef5adb050bab753e46f120491a6e04896b
Henryque Pimenta <pimenta.henry@gmail.com>
2025-12-02 10:32:08 -0300

    Primeira Etapa Completa, implementado: Exibir, Ordenar, Atualizar, Salvar, Carregar Ranking

--- include/ranking.h
+++ include/ranking.h
@@ -1,1 +1,21 @@
 // funcoes e structs do ranking
+#ifndef RANKING_H
+#define RANKING_H
+
+typedef struct{
+    char nome[50];
+    int pontos;
+}Jogador;
+
+Jogador* carregarRanking(const char *arquivo, int *qtd);
+
+int salvarRanking(const char *arquivo, const Jogador *ranking, int qtd);
+
+int atualizarRanking(Jogador **ranking, int *qtd, Jogador novoJogador);
+
+void ordenarRanking(Jogador *ranking, int qtd);
+
+void exibirRanking(const Jogador *ranking, int qtd);
+
+#endif
+
--- src/ranking.c
+++ src/ranking.c
@@ -1,1 +1,33 @@
-// acoes relacionadas aso arquivo
+// acoes relacionadas aos arquivo
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ranking.h"
+
+Jogador* carregarRanking(const char *arquivo, int *qtd) {
+    FILE *file;
+    char linhas[100];
+    int contador = 0, i = 0;
+
+    file = fopen(arquivo, "r");
+    if(file == NULL){
+        *qtd = 0;
+        return NULL;
+    }
+
+    while(fgets(linhas, sizeof(linhas), file) != NULL) { 
+        contador++;    
+    }
+
+    rewind(arquivo);
+
+    Jogador *jogadores = malloc(contador *sizeof(Jogador));
+    while (fscanf(file, " %49s %d", jogadores[i].nome, &jogadores[i].pontos) == 2) {
+        i++;
+    }
+
+    fclose(file);
+    *qtd = contador;
+    return jogadores;
+}
+
